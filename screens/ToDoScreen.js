7b11bc91d349ca175e5455b7c18854985323689a
Foucsi <julien.foucartreact@gmail.com>
2022-12-12 10:47:36 +0100

    update screens

---
+++ screens/ToDoScreen.js
@@ -0,0 +1,8 @@
+import { View, StyleSheet, Text } from "react-native";
+
+// get file data toDoData.json
+const dataCheckList = require("../toDoData.json");
+
+export default function ToDoScreen() {
+  return <View></View>;
+}
