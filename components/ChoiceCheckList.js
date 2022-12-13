f64f0c9aa193495d474e6e0d913b6ab7fdd68c24
Foucsi <julien.foucartreact@gmail.com>
2022-12-13 09:07:09 +0100

    updtate screen julien F

---
+++ components/ChoiceCheckList.js
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+export default function ChoiceCheckList({ props, setDataId }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.containerCard}>
+        <Pressable onPress={() => setDataId(props.id)}>
+          <FontAwesome5 name={props.icone} size={36} color="#fff" />
+        </Pressable>
+      </View>
+      <Text style={{ textAlign: "center", color: "#7F7F7F", fontSize: 12 }}>
+        {props.name.toUpperCase()}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "space-around",
+    marginRight: 15,
+  },
+  containerCard: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 90,
+    width: 90,
+    borderRadius: "50%",
+    backgroundColor: "#21A37C",
+  },
+});
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { View, StyleSheet, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <FontAwesome name="user-circle-o" size={38} color="#fff" />
+      <Text>Welcome Julien !</Text>
+      <Feather name="menu" size={32} color="#fff" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#21A37C",
+    flexDirection: "row",
+    width: "100%",
+    height: "10%",
+    padding: 10,
+    marginTop: 50,
+  },
+});
