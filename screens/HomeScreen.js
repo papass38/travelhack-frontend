b0165f44815648673070514cc6268d5a495b3936
etienne <e.papassin@gmail.coml>
2022-12-13 11:10:20 +0100

    maj navigation et sign in

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,44 @@
-import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function HomeScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-            style={styles.button}
-            activeOpacity={0.8}
-            onPress={() => navigation.navigate('Profil')}>
-            <Text>Profil</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-            style={styles.button}
-            activeOpacity={0.8}
-            onPress={() => navigation.navigate('CountrySearch')}>
-            <Text>New Trip</Text>
-        </TouchableOpacity>
-        </View>
-    )
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate("Profil")}
+      >
+        <Text>Profil</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate("CountrySearch")}
+      >
+        <Text>New Trip</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-        display: 'flex',
-        alignItems: 'center',
-        paddingTop: 15,
-        paddingBottom: 15,
-        width: '80%',
-        backgroundColor: 'red',
-        borderRadius: 10,
-    },
-    textButton: {
-        color: 'white',
-        fontSize: 20,
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingBottom: 15,
+    width: "80%",
+    backgroundColor: "#20B08E",
+    borderRadius: 10,
+  },
+  textButton: {
+    color: "white",
+    fontSize: 20,
+  },
+});
