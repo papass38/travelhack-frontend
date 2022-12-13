352c79d31a43b9e1941280450efae8fdbd37d7d8
Youness <y.ouzzedi@hotmail.com>
2022-12-13 09:09:17 +0100

    welcome

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,60 +1,4 @@
-<<<<<<< HEAD
-import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function WelcomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.8}
-        onPress={() => navigation.navigate("Sign in")}
-      >
-        <Text style={styles.textButton}>Slide to access Travelhack</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    display: "flex",
-    alignItems: "center",
-    paddingTop: 15,
-    paddingBottom: 15,
-    width: "80%",
-    backgroundColor: "#20B08E",
-    borderRadius: 10,
-  },
-  textButton: {
-    color: "white",
-    fontSize: 20,
-  },
-  signInBtn: {
-    display: "flex",
-    alignItems: "center",
-    paddingTop: 15,
-    paddingBottom: 15,
-    width: "80%",
-    backgroundColor: "black",
-    borderRadius: 10,
-  },
-  signupBtn: {
-    display: "flex",
-    alignItems: "center",
-    paddingTop: 15,
-    paddingBottom: 15,
-    width: "80%",
-    backgroundColor: "black",
-    borderRadius: 10,
-  },
-});
-=======
 import { Button, StyleSheet, Text, View, TouchableOpacity, ImageBackground, SafeAreaView } from 'react-native';
 
 export default function WelcomeScreen({ navigation }) {
@@ -110,4 +54,3 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
 });
->>>>>>> aab566ee1337aad1c93c181ce5871114b1943251
