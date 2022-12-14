432de0da2d360bf1aba9802db1f092efded882e7
Youness <y.ouzzedi@hotmail.com>
2022-12-14 09:30:31 +0100

    profil navigation

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Header({ navigation }) {
         name="user-circle-o"
         size={38}
         color="#fff"
-        onPress={() => navigation.navigate("TabNavigator")}
+        onPress={() => navigation.navigate("Profil")}
       />
       <Text>Welcome {user.username}</Text>
       <Text>Travel : {trip.initialDestination.adress}</Text>
