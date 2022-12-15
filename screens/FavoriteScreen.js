bd1059de0b6f99f20e038b8d32597a9cc15592d2
Foucsi <julien.foucartreact@gmail.com>
2022-12-15 15:57:15 +0100

    commit

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -5,7 +5,7 @@ import {
   View,
   TouchableOpacity,
   SafeAreaView,
-  Pressable
+  Pressable,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -14,22 +14,16 @@ export default function FavoriteScreen({ navigation }) {
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
         style={styles.header}
-        onPress={() => navigation.navigate("Profil")}>
-        <Ionicons
-          name="chevron-back"
-          size={50}
-          color="#20B08E"
-        />
+        onPress={() => navigation.navigate("Profil")}
+      >
+        <Ionicons name="chevron-back" size={50} color="#20B08E" />
         <Text style={styles.text}>Profil</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.header}
-        onPress={() => navigation.navigate("Wishlist")}>
-        <Ionicons
-          name="ios-heart"
-          size={50}
-          color="red"
-        />
+        onPress={() => navigation.navigate("Wishlist")}
+      >
+        <Ionicons name="ios-heart" size={50} color="red" />
         <Text style={styles.text}>Wishlist</Text>
       </TouchableOpacity>
     </SafeAreaView>
@@ -44,7 +38,7 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    width: '35%',
+    width: "35%",
   },
   text: {
     color: "#20B08E",
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -42,7 +42,7 @@ export default function SignUpScreen({ navigation }) {
   const [signUpEmail, setSignUpEmail] = useState("");
 
   const handleRegister = () => {
-    fetch("http://172.16.191.12:3000/users/signup", {
+    fetch("http://172.16.190.137:3000/users/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
