6669ca96d5759b4f9961c2db3abb3a7367cc861b
Foucsi <julien.foucartreact@gmail.com>
2022-12-16 09:21:15 +0100

    commit

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -5,33 +5,39 @@ import {
   View,
   TouchableOpacity,
   SafeAreaView,
-  Pressable
+  Pressable,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function FavoriteScreen({ navigation }) {
+  const user = useSelector((state) => state.user.value);
+  const [infoUser, setInfoUser] = useState({ username: null, email: null });
+
+  useEffect(() => {
+    fetch(`http://172.16.190.137:3000/users/${user.username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.result) {
+          setInfoUser({ username: data.user.username, email: data.user.email });
+        }
+      });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
         style={styles.header}
-        onPress={() => navigation.navigate("Profil")}>
-        <Ionicons
-          name="chevron-back"
-          size={50}
-          color="#20B08E"
-        />
+        onPress={() => navigation.navigate("Profil")}
+      >
+        <Ionicons name="chevron-back" size={50} color="#20B08E" />
         <Text style={styles.text}>Profil</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.header}
-        onPress={() => navigation.navigate("Wishlist")}>
-        <Ionicons
-          name="ios-heart"
-          size={50}
-          color="red"
-        />
-        <Text style={styles.text}>Wishlist</Text>
-      </TouchableOpacity>
+      <Text>Username :{infoUser.username}</Text>
+      <Text>Email : {infoUser.email}</Text>
     </SafeAreaView>
   );
 }
@@ -44,7 +50,7 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    width: '35%',
+    width: "35%",
   },
   text: {
     color: "#20B08E",
