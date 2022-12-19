d5febbd1777cfc0d053916066472f2074ce49c4e
Ryan <ramoulryan@gmail.com>
2022-12-19 15:33:24 +0100

    HomeScreen v1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,9 +9,123 @@ import {
 import Header from "../components/Header";
 import { AntDesign } from "@expo/vector-icons";
 
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import fecthIp from "../fetchIp.json";
+
+import Modal from "react-native-modal";
+
 export default function HomeScreen({ navigation }) {
+  const myUsername = useSelector((state) => state.user.value.username);
+
+  const [newTripsList, setNewTripsList] = useState([]);
+  const [oldTripsList, setOldTripsList] = useState([]);
+
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [modalData, setModalData] = useState({
+    destination: "Londres",
+    startDate: "20/12/2022",
+    endDate: "27/12/2022",
+    steps: [{ name: "Manchester" }, { name: "Chelsea" }, { name: "Liverpool" }],
+    totalBudget: "522.19",
+  });
+
+  useEffect(() => {
+    fetch(`http://${fecthIp.myIp}:3000/users/alltrips/${myUsername}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setNewTripsList(data.trips);
+      });
+  }, []);
+
+  let oldTripsExist = "No old trip yet";
+  if (oldTripsList.length > 0) {
+    oldTripsExist = "Old Trips";
+  }
+
+  let newTripsExist = "No trip planned yet";
+  if (newTripsList.length > 0) {
+    newTripsExist = "";
+  }
+
+  const newTripsDisplay = newTripsList.map((data, i) => {
+    return (
+      <TouchableOpacity
+        key={i}
+        style={styles.newTrip}
+        onPress={() => {
+          setModalVisible(!isModalVisible);
+          setModalData({
+            destination: data.destination,
+            startDate: data.startDate,
+            endDate: data.endDate,
+            steps: data.steps,
+            totalBudget: data.totalBudget,
+          });
+        }}
+      >
+        <Image
+          style={styles.imgTrip}
+          source={{
+            uri: "https://img.static-rmg.be/a/view/q75/w4000/h2667/4779070/2135445-jpg.jpg",
+          }}
+        />
+
+        <View style={styles.textNewTrip}>
+          <View>
+            <Text style={{ fontWeight: "bold" }}>{data.destination}</Text>
+            <Text>{new Date(data.startDate).toLocaleDateString()}</Text>
+          </View>
+          <AntDesign name="info" size={20} color="black" />
+        </View>
+      </TouchableOpacity>
+    );
+  });
+
+  const oldTripsDisplay = oldTripsList.map((data, i) => {
+    return (
+      <View key={i} style={styles.oldTrip}>
+        <Image
+          style={styles.imgOldTrip}
+          source={{
+            uri: data.img,
+          }}
+        />
+
+        <View style={styles.textOldTrip}>
+          <View>
+            <Text style={{ fontWeight: "bold" }}>{data.country}</Text>
+            <Text>{data.date}</Text>
+          </View>
+          <AntDesign name="info" size={20} color="black" />
+        </View>
+      </View>
+    );
+  });
+
+  const stepsModal = modalData.steps.map((data, i) => {
+    return <Text key={i}>{data.name}</Text>;
+  });
+
   return (
     <View style={styles.container}>
+      <Modal visible={isModalVisible} style={styles.modal}>
+        <View style={styles.modalContent}>
+          <Text>{modalData.destination}</Text>
+          <Text>{modalData.startDate}</Text>
+          <Text>{modalData.endDate}</Text>
+          <Text>{modalData.totalBudget}€</Text>
+          {stepsModal}
+          <TouchableOpacity
+            onPress={() => setModalVisible(!isModalVisible)}
+            style={styles.modalButton}
+          >
+            <Text style={styles.buttonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
       <Header navigation={navigation} />
       <View>
         <TouchableOpacity
@@ -23,96 +137,34 @@ export default function HomeScreen({ navigation }) {
         </TouchableOpacity>
       </View>
 
-      <ScrollView horizontal contentContainerStyle={styles.newTripsScroll}>
-        <View>
-          <Image
-            source={{
-              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
-            }}
-            style={styles.imgNewTrip}
-          />
-          <View style={styles.textContainerNewTrip}>
-            <View>
-              <Text>Mexico</Text>
-              <Text>En cours</Text>
-            </View>
-            <View>
-              <AntDesign name="edit" size={24} color="black" />
-            </View>
-          </View>
-        </View>
-        <View>
-          <Image
-            source={{
-              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
-            }}
-            style={styles.imgNewTrip}
-          />
-          <View style={styles.textContainerNewTrip}>
-            <View>
-              <Text>Mexico</Text>
-              <Text>En cours</Text>
-            </View>
-            <View>
-              <AntDesign name="edit" size={24} color="black" />
-            </View>
-          </View>
-        </View>
-        <View>
-          <Image
-            source={{
-              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
-            }}
-            style={styles.imgNewTrip}
-          />
-          <View style={styles.textContainerNewTrip}>
-            <View>
-              <Text>Mexico</Text>
-              <Text>En cours</Text>
-            </View>
-            <View>
-              <AntDesign name="edit" size={24} color="black" />
-            </View>
-          </View>
-        </View>
-        <View>
-          <Image
-            source={{
-              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
-            }}
-            style={styles.imgNewTrip}
-          />
-          <View style={styles.textContainerNewTrip}>
-            <View>
-              <Text>Mexico</Text>
-              <Text>En cours</Text>
-            </View>
-            <View>
-              <AntDesign name="edit" size={24} color="black" />
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-
-      <ScrollView contentContainerStyle={styles.oldTripsScroll}>
-        <View style={styles.oldTripsContainer}>
-          <Image
-            source={{
-              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
-            }}
-            style={styles.imgOldTrip}
-          />
-          <View style={styles.textContainerOldTrip}>
-            <View>
-              <Text>Mexico</Text>
-              <Text>En cours</Text>
-            </View>
-            <View>
-              <AntDesign name="edit" size={24} color="black" />
-            </View>
-          </View>
-        </View>
-      </ScrollView>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 20,
+        }}
+      >
+        {newTripsExist}
+      </Text>
+      <View style={styles.newScrollContainer}>
+        <ScrollView horizontal contentContainerStyle={styles.newTripsScroll}>
+          {newTripsDisplay}
+        </ScrollView>
+      </View>
+      <Text
+        style={{
+          fontWeight: "bold",
+          marginTop: 20,
+          marginBottom: 20,
+          fontSize: 20,
+        }}
+      >
+        {oldTripsExist}
+      </Text>
+      <View style={styles.OldScrollContainer}>
+        <ScrollView contentContainerStyle={styles.oldTripsScroll}>
+          {oldTripsDisplay}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -126,35 +178,95 @@ const styles = StyleSheet.create({
   newTripBtn: {
     flexDirection: "row",
     alignItems: "center",
-    margin: 10,
+    margin: 30,
   },
   text: {
     fontSize: 30,
     marginLeft: 10,
     fontWeight: "bold",
   },
+  newScrollContainer: {
+    height: "30%",
+    minWidth: "100%",
+  },
   newTripsScroll: {
     flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  imgNewTrip: {
+  newTrip: {
+    height: 190,
     width: 200,
-    height: 150,
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "grey",
+    marginHorizontal: 10,
+    borderRadius: 10,
+    overflow: "hidden",
   },
-  textContainerNewTrip: {
+  imgTrip: {
+    width: "100%",
+    height: "70%",
+  },
+  textNewTrip: {
     flexDirection: "row",
     justifyContent: "space-between",
-
-    borderWidth: 1,
-    borderTopWidth: 0,
+    alignItems: "center",
+    margin: 10,
   },
-  oldTripsScroll: {},
-  oldTripsContainer: {
-    flexDirection: "row",
-    borderWidth: 1,
+  OldScrollContainer: {
+    height: "30%",
     width: "100%",
   },
+  oldTripsScroll: {
+    alignItems: "center",
+  },
+  oldTrip: {
+    height: 100,
+    width: "90%",
+    borderWidth: 1,
+    borderColor: "grey",
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
   imgOldTrip: {
+    width: "40%",
+    height: "100%",
+  },
+  textOldTrip: {
+    width: "60%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  modal: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    margin: 0,
+    backgroundColor: "rgba(27, 25, 26, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    height: "80%",
+    width: "80%",
+    padding: 20,
+    borderRadius: 20,
+  },
+  modalButton: {
+    marginTop: 10,
+    backgroundColor: "#20B08E",
+    justifyContent: "center",
+    alignItems: "center",
     width: 100,
-    height: 80,
+    height: "10%",
+    borderRadius: 5,
   },
 });
