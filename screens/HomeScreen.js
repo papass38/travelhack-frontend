6efd5babf7da031c5de3b54412c20bbba0343190
Ryan <ramoulryan@gmail.com>
2022-12-19 09:22:09 +0100

    refresh

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,13 @@
-import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import Header from "../components/Header";
 import { AntDesign } from "@expo/vector-icons";
-
-import SwipeButton from 'rn-swipe-button'
 
 export default function HomeScreen({ navigation }) {
   return (
@@ -17,6 +22,97 @@ export default function HomeScreen({ navigation }) {
           <Text style={styles.text}>New trip</Text>
         </TouchableOpacity>
       </View>
+
+      <ScrollView horizontal contentContainerStyle={styles.newTripsScroll}>
+        <View>
+          <Image
+            source={{
+              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
+            }}
+            style={styles.imgNewTrip}
+          />
+          <View style={styles.textContainerNewTrip}>
+            <View>
+              <Text>Mexico</Text>
+              <Text>En cours</Text>
+            </View>
+            <View>
+              <AntDesign name="edit" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+        <View>
+          <Image
+            source={{
+              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
+            }}
+            style={styles.imgNewTrip}
+          />
+          <View style={styles.textContainerNewTrip}>
+            <View>
+              <Text>Mexico</Text>
+              <Text>En cours</Text>
+            </View>
+            <View>
+              <AntDesign name="edit" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+        <View>
+          <Image
+            source={{
+              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
+            }}
+            style={styles.imgNewTrip}
+          />
+          <View style={styles.textContainerNewTrip}>
+            <View>
+              <Text>Mexico</Text>
+              <Text>En cours</Text>
+            </View>
+            <View>
+              <AntDesign name="edit" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+        <View>
+          <Image
+            source={{
+              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
+            }}
+            style={styles.imgNewTrip}
+          />
+          <View style={styles.textContainerNewTrip}>
+            <View>
+              <Text>Mexico</Text>
+              <Text>En cours</Text>
+            </View>
+            <View>
+              <AntDesign name="edit" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      <ScrollView contentContainerStyle={styles.oldTripsScroll}>
+        <View style={styles.oldTripsContainer}>
+          <Image
+            source={{
+              uri: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2019/07/Organiser-son-road-trip.png",
+            }}
+            style={styles.imgOldTrip}
+          />
+          <View style={styles.textContainerOldTrip}>
+            <View>
+              <Text>Mexico</Text>
+              <Text>En cours</Text>
+            </View>
+            <View>
+              <AntDesign name="edit" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -36,5 +132,29 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginLeft: 10,
     fontWeight: "bold",
+  },
+  newTripsScroll: {
+    flexDirection: "row",
+  },
+  imgNewTrip: {
+    width: 200,
+    height: 150,
+  },
+  textContainerNewTrip: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+    borderWidth: 1,
+    borderTopWidth: 0,
+  },
+  oldTripsScroll: {},
+  oldTripsContainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    width: "100%",
+  },
+  imgOldTrip: {
+    width: 100,
+    height: 80,
   },
 });
