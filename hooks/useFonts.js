090cc5b8e0ebaae6a3d732bc4a47eadc11459c83
Youness <y.ouzzedi@hotmail.com>
2022-12-22 11:01:51 +0100

    font refresh

--- hooks/useFonts.js
+++ hooks/useFonts.js
@@ -3,5 +3,6 @@ import * as Font from "expo-font";
 export default useFonts = async () => {
    await Font.loadAsync({
       "Ubuntu-Regular": require('../assets/fonts/Ubuntu-Regular.ttf'),
+      'Ubuntu-Bold': require('../assets/fonts/Ubuntu-Bold.ttf')
     });
 };
